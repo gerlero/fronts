dece20bf3e8f4cd25ec782ea5cc6289895302624
Gabriel Gerlero <gerlero@users.noreply.github.com>
2019-12-26 11:58:10 -0300

    Bump version to 0.9.6-dev

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Gabriel S. Gerlero'
 author = 'Gabriel S. Gerlero'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.5'
+release = '0.9.6-dev'
 
 
 # -- General configuration ---------------------------------------------------
