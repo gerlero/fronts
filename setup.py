0f0cabcb4fa2afc7d4067c71aaba2cc15054f048
Gabriel Gerlero <gerlero@users.noreply.github.com>
2020-05-12 15:21:36 -0300

    Rearrange setup.py

--- setup.py
+++ setup.py
@@ -4,32 +4,17 @@ import setuptools
 
 from io import open
 
-requirements = ('scipy>=1.0.0', 'numpy')
-
-classifiers = ('Development Status :: 4 - Beta',
-			   'Intended Audience :: Science/Research',
-			   'License :: OSI Approved :: BSD License',
-			   'Programming Language :: Python',
-			   'Programming Language :: Python :: 2.7',
-			   'Programming Language :: Python :: 3',
-               'Programming Language :: Python :: 3.5',
-			   'Programming Language :: Python :: 3.6',
-			   'Programming Language :: Python :: 3.7',
-               'Programming Language :: Python :: 3.8',
-			   'Topic :: Scientific/Engineering :: Physics',
-			   'Topic :: Software Development :: Libraries',
-			   'Operating System :: OS Independent')
-
-with open("README.md", 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+with open("README.md", 'r', encoding='utf-8') as f:
+    readme = f.read()
 
 setuptools.setup(
     name='fronts',
     version='0.9.8-dev',
     author="Gabriel S. Gerlero",
+    author_email="ggerlero@cimec.unl.edu.ar",
     description="Numerical library for nonlinear diffusion problems based on " 
                 "the Boltzmann transformation.",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type='text/markdown',
     url="https://github.com/gerlero/fronts",
     project_urls={
@@ -37,9 +22,21 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/gerlero/fronts/issues",
         "Source Code": "https://github.com/gerlero/fronts",
     },
-    install_requires=requirements,
-    packages=('fronts',),
+    packages=['fronts'],
     license='BSD',
-    classifiers=classifiers,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Topic :: Software Development :: Libraries',
+                 'Operating System :: OS Independent'],
+    install_requires=['scipy>=1.0.0', 'numpy'],
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*'
 )
