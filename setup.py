527a4ad59675e415ddee9ef972fc1264fbb777ad
Gabriel Gerlero <gerlero@users.noreply.github.com>
2020-05-12 15:13:43 -0300

    Add universal parameter for bdist_wheel in setup.py

--- setup.py
+++ setup.py
@@ -53,5 +53,6 @@ setuptools.setup(
                  'Operating System :: OS Independent'],
     install_requires=['scipy>=1.0.0', 'numpy'],
     extras_require=extras,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*'
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    options={"bdist_wheel": {"universal": "1"}}
 )
