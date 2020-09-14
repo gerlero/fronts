d097516b025fec6a89e0e0c74f17531ef83eb014
Gabriel Gerlero <gerlero@users.noreply.github.com>
2020-09-14 12:00:46 -0300

    Fix code indentation format

--- fronts/__init__.py
+++ fronts/__init__.py
@@ -7,10 +7,10 @@ from __future__ import division, absolute_import, print_function
 
 from ._boltzmann import ode, BaseSolution, o, do_dr, do_dt, r, t, as_o
 from ._semiinfinite import (solve, solve_flowrate, solve_from_guess, Solution, 
-							inverse)
+                            inverse)
 
 __all__ = ['solve', 'solve_flowrate', 'solve_from_guess', 'Solution',
-		   'inverse', 'ode', 'BaseSolution', 'o', 'do_dr', 'do_dt', 'r', 't',
-		   'as_o']
+           'inverse', 'ode', 'BaseSolution', 'o', 'do_dr', 'do_dt', 'r', 't',
+           'as_o']
 
 __version__ = '0.9.9-dev0'  # Single source of the package's version
