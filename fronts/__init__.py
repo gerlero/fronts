0b81cf3fbe316187cd85bdb79a625899db68348d
Gabriel Gerlero <gerlero@users.noreply.github.com>
2021-05-13 17:46:19 -0300

    Migrate from setup.py to setup.cfg

--- fronts/__init__.py
+++ fronts/__init__.py
@@ -5,6 +5,16 @@ transformation.
 
 from __future__ import division, absolute_import, print_function
 
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:  # No importlib.metadada in Python < 3.8
+    import importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version('fronts')
+except Exception:
+    __version__ = 'unknown'
+
 from ._boltzmann import ode, BaseSolution, o, do_dr, do_dt, r, t, as_o
 from ._semiinfinite import (solve, solve_flowrate, solve_from_guess, Solution, 
                             inverse)
@@ -13,4 +23,4 @@ __all__ = ['solve', 'solve_flowrate', 'solve_from_guess', 'Solution',
            'inverse', 'ode', 'BaseSolution', 'o', 'do_dr', 'do_dt', 'r', 't',
            'as_o']
 
-__version__ = '0.9.11-dev0'  # Single source of the package's version
+
