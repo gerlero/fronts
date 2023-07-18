6affe8690eecd20af9664ed49204fe255ead3bc5
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-07-18 20:46:26 -0300

    Move source of package version to __init__.py

--- fronts/__init__.py
+++ fronts/__init__.py
@@ -3,15 +3,7 @@ Numerical library for nonlinear diffusion problems based on the Boltzmann
 transformation.
 """
 
-try:
-    import importlib.metadata as importlib_metadata
-except ImportError:  # No importlib.metadada in Python < 3.8
-    import importlib_metadata
-
-try:
-    __version__ = importlib_metadata.version('fronts')
-except Exception:
-    __version__ = 'unknown'
+__version__ = '0.1.0'
 
 from ._boltzmann import ode, BaseSolution, o, do_dr, do_dt, r, t, as_o
 from ._semiinfinite import (solve, solve_flowrate, solve_from_guess, Solution, 
@@ -20,5 +12,3 @@ from ._semiinfinite import (solve, solve_flowrate, solve_from_guess, Solution,
 __all__ = ['solve', 'solve_flowrate', 'solve_from_guess', 'Solution',
            'inverse', 'ode', 'BaseSolution', 'o', 'do_dr', 'do_dt', 'r', 't',
            'as_o']
-
-
