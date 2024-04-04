2cdadfc15ec7f593b5909d8c76c8dbb0963f58ce
Gabriel Gerlero <gerlero@users.noreply.github.com>
2024-04-04 18:34:28 -0300

    Fix exported symbols

--- fronts/__init__.py
+++ fronts/__init__.py
@@ -10,5 +10,5 @@ from ._semiinfinite import solve, solve_flowrate, solve_from_guess, Solution
 from ._inverse import inverse, sorptivity
 
 __all__ = ['solve', 'solve_flowrate', 'solve_from_guess', 'Solution',
-           'inverse', 'sorptivity' 'ode', 'BaseSolution', 'o', 'do_dr',
+           'inverse', 'sorptivity', 'ode', 'BaseSolution', 'o', 'do_dr',
            'do_dt', 'r', 't', 'as_o']
