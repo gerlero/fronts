a86451c8f564c67962dc461d6ff3b5d4e9d7c1d3
Gabriel Gerlero <gerlero@users.noreply.github.com>
2025-11-21 00:39:38 -0300

    Switch to uv build backend

--- fronts/__init__.py
+++ fronts/__init__.py
@@ -1,10 +1,12 @@
 """Numerical library for nonlinear diffusion problems based on the Boltzmann transformation."""
 
-__version__ = "1.2.7"
+from importlib.metadata import version
 
 from ._boltzmann import BaseSolution, as_o, do_dr, do_dt, o, ode, r, t
 from ._inverse import inverse, sorptivity
 from ._semiinfinite import Solution, solve, solve_flowrate, solve_from_guess
+
+__version__ = version("fronts")
 
 __all__ = [
     "BaseSolution",
